cc77e7b8e23be89efa586a5ce56e9462fb106258
nishantmadu <nishant.madu@gmail.com>
2016-06-01 12:19:56 +0530

    Added custom services in match detail page to fetch the API from two different links of API

--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,17 @@ angularApp.config(function ($routeProvider) {
     controller: 'DetailsController',
     controllerAs: 'dc'
   })
+
+
+// .when ('/leaguedetails/:uniqId', {
+//   templateUrl: 'pages/details.html',
+//   controller: 'PointsController',
+//   controllerAs: 'pc'
+// })
+//
 });
+
+
 
 angularApp.config(['$httpProvider', function ($httpProvider) {
    $httpProvider.defaults.headers.common['X-Auth-Token'] = 'e03b9e6e71734ae38c076a13eac738ea';
@@ -45,14 +55,43 @@ angularApp.controller("HomeController",['$resource','$filter', '$http','$q',func
 }]);
 
 
-angularApp.controller("DetailsController",['$resource','$filter','$routeParams',
-function($resource,$filter,$routeParams){
+angularApp.controller("DetailsController",['$filter','$routeParams','FootballService',function($filter,$routeParams,FootballService){
     var vm=this;
 
     var id = $routeParams.uniqId;
-    var footballDetails = $resource('http://api.football-data.org/v1/soccerseasons/' + id + '/teams');
-    vm.detailsResponse = footballDetails.get();
+    vm.detailsResponse = FootballService.getTeams(id);
+    vm.pointsResponse = FootballService.getPoints(id);
+
     console.log(vm.detailsResponse);
+    console.log(vm.pointsResponse);
+
+  }]);
 
 
-}]);
+//     angularApp.controller("PointsController",['$resource','$filter','$routeParams',
+//     function($resource,$filter,$routeParams){
+//       var vm=this;
+//         var footballResource = $resource('http://api.football-data.org/v1/soccerseasons/');
+//         vm.footballResponse = footballResource.query();
+//
+// }]);
+
+
+
+
+angularApp.service('FootballService', function($resource) {
+  var vm =this;
+    vm.getTeams = function (id) {
+      var footballDetails = $resource('http://api.football-data.org/v1/soccerseasons/' + id + '/teams');
+      vm.detailsResponse = footballDetails.get();
+      return vm.detailsResponse;
+    }
+
+    vm.getPoints = function(id) {
+      var footballDetails = $resource('http://api.football-data.org/v1/soccerseasons/' + id + '/fixtures');
+      vm.pointsResponse = footballDetails.query();
+      return vm.pointsResponse;
+    }
+
+
+});
