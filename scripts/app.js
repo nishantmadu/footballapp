fd30fb08e23488ac1af19429f5d59911467a408f
nishantmadu <nishant.madu@gmail.com>
2016-06-01 10:33:21 +0530

    Landing Page is initialized few bits of detail page in progress

--- scripts/app.js
+++ scripts/app.js
@@ -7,37 +7,52 @@ angularApp.config(function ($routeProvider) {
     controller: 'HomeController',
     controllerAs: 'vh'
   })
+
+  .when ('/leaguedetails/:uniqId', {
+    templateUrl: 'pages/details.html',
+    controller: 'DetailsController',
+    controllerAs: 'dc'
+  })
 });
-//   .when ('/matchdetails/:uniqId', {
-//     templateUrl: 'templates/details.html',
-//     controller: 'DetailsController',
-//     controllerAs: 'dc'
-//   })
-// });
+
+angularApp.config(['$httpProvider', function ($httpProvider) {
+   $httpProvider.defaults.headers.common['X-Auth-Token'] = 'e03b9e6e71734ae38c076a13eac738ea';
+}]);
 
 
-angularApp.controller("HomeController",['$resource','$filter', '$http',function($resource,$filter,$http){
+angularApp.controller("HomeController",['$resource','$filter', '$http','$q',function($resource,$filter,$http,$q){
   var vm=this;
-    // var footballResource = $resource('http://api.football-data.org/v1/soccerseasons/');
-    // vm.footballResponse = footballResource.get();
-      var config = {headers:  {
-        'X-Auth-Token': 'e03b9e6e71734ae38c076a13eac738ea'
-      }
-    };
+    var footballResource = $resource('http://api.football-data.org/v1/soccerseasons/');
+    vm.footballResponse = footballResource.query();
+    console.log(vm.footballResponse);
 
-    var data = $http.get("http://api.football-data.org/v1/soccerseasons/", config);
-    console.log(data);
+    // $http.get("http://api.football-data.org/v1/soccerseasons/")
+    //     .then(function(response){
+    //       if (typeof response.data === 'object') {
+    //                         vm.footballResponse = response.data;
+    //                     } else {
+    //                         return $q.reject(response.data);
+    //                     }},
+    //         function(response) {
+    //           return $q.reject(response.data);
+    //                     });
+
+
+
+
 
 
 }]);
 
 
-// angularApp.controller("DetailsController",['$resource','$filter','$routeParams',
-// function($resource,$filter,$routeParams){
-//     var vm=this;
-//     var id = $routeParams.uniqId;
-//     var cricDetails = $resource('http://cricapi.com/api/cricketScore',{unique_id:id});
-//     vm.cricResponse = cricDetails.get();
-//
-//
-// }]);
+angularApp.controller("DetailsController",['$resource','$filter','$routeParams',
+function($resource,$filter,$routeParams){
+    var vm=this;
+
+    var id = $routeParams.uniqId;
+    var footballDetails = $resource('http://api.football-data.org/v1/soccerseasons/' + id + '/teams');
+    vm.detailsResponse = footballDetails.get();
+    console.log(vm.detailsResponse);
+
+
+}]);
